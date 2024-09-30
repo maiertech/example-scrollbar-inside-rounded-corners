3b3b4ce219b28b224ce71349b0c968a2e90a3c0c
Thilo Maier <git@maier.asia>
2024-09-30 22:58:04 +0200

    Create a list of books where scrollbar messes with rounded corners

--- README.md
+++ README.md
@@ -1,20 +1,7 @@
-# template-sveltekit-example
+# Scrollbars inside a container with rounded corners
 
-This is a [SvelteKit](https://kit.svelte.dev/) skeleton project which I use as a [GitHub template](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) to create SvelteKit examples.
+## Challenge
 
-## Keeping dependencies up-to-date
+I have created a simple book list with rounded corners. But the scrollbar goes right through the rounded corners. How can I fix this?
 
-I update dependencies in this template regularly. These are the steps:
-
-1. Run `pnpm create svelte@latest .` to regenerate the SvelteKit skeleton project into the existing directory.
-1. **Where should we create your project?** Hit enter to use the current directory.
-1. **Directory not empty. Continue?** Yes.
-1. **Which Svelte app template?** Skeleton project.
-1. **Add type checking with TypeScript?** Yes, using TypeScript syntax.
-1. Add ESLint, Prettier and Svelte 5.
-1. Revert `README.md`.
-1. Delete folder `node_modules` and file `pnpm-lock.yaml` and run `pnpm i`.
-
-## Debugging
-
-Just launch the application in the [JavaScript Debug Terminal](https://code.visualstudio.com/docs/editor/debugging#_launch-javascript-debug-terminal).
+## Solution
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const books = [
+		{ title: 'The Whisper of Shadows', author: 'Eliza Blackwood' },
+		{ title: 'Quantum Echoes', author: 'Dr. Aiden Frost' },
+		{ title: 'The Last Serenade', author: 'Sophia Nightingale' },
+		{ title: 'Neon Dreams in Tokyo', author: 'Hiro Tanaka' },
+		{ title: 'The Alchemy of Starlight', author: 'Luna Silverstone' },
+		{ title: 'Echoes from the Void', author: 'Commander Jack Nova' },
+		{ title: 'Whispers of the Ancient Forest', author: 'Aria Greenleaf' },
+		{ title: 'The Clockwork Conspiracy', author: 'Professor Thaddeus Gearhart' },
+		{ title: 'Sands of Time: A Desert Odyssey', author: 'Zara Dune' },
+		{ title: 'The Quantum Tailor', author: 'Dr. Evelyn Threadcraft' },
+		{ title: 'Melodies of the Heart', author: 'Maestro Giovanni Allegro' },
+		{ title: 'The Cybernetic Sonata', author: 'Ada Lovelace II' },
+		{ title: 'Secrets of the Celestial Sea', author: 'Captain Orion Stardust' },
+		{ title: 'The Whimsical Watchmaker', author: 'Horatio Ticktock' }
+	];
+</script>
+
+<ul>
+	{#each books as book}
+		<li>
+			<span class="title">{book.title}</span>
+			<span class="author">by {book.author}</span>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+
+		width: fit-content;
+		max-height: 300px;
+		margin: 0;
+		padding: 1rem;
+		border: 2px solid black;
+		border-radius: 24px;
+
+		overflow-y: auto;
+		scrollbar-color: black lightgray;
+	}
+
+	li {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.title {
+		font-weight: bold;
+	}
+
+	.author {
+		font-style: italic;
+	}
+</style>
